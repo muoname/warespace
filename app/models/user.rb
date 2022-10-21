cf368ae5c1c58a9e953da459bf6e5a81f2eb3f27
regina-sv <rvibar@gbox.adnu.edu.ph>
2022-10-21 17:52:23 +0800

    set user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
