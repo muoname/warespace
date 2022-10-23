f961b66c2a799e0dbf2e64e97f8d7b8dc576ec35
regina-sv <rvibar@gbox.adnu.edu.ph>
2022-10-23 17:45:42 +0800

    define methods to resize space images

--- app/models/space.rb
+++ app/models/space.rb
@@ -8,4 +8,21 @@ class Space < ApplicationRecord
     # many images attached
     has_many_attached :pictures
 
+
+    # method to resize hero image on _space.html.erb
+    def image_as_hero
+        
+        image.variant(resize_to_limit: [300, 300]).processed
+        
+    end
+
+    # method to resize multiple image as thumbnails on _space.html.erb
+    def pictures_as_thumb
+
+        pictures.map do |pic|
+            pic.variant(resize_to_limit: [150, 150]).processed
+        end
+
+    end
+
 end
