c9ed1965c2924646c8085f3a55aa0e5d3d3c13c3
Muoname <easpe@gbox.adnu.edu.ph>
2022-11-20 01:53:10 +0800

    Frontend Update: Listing and Show pages WIP

--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory  } from 'vue-router'
 import LandingPage from "./views/shared/landing.vue"
 import Registration from "./views/shared/registration.vue"
 import HostIndex from "./views/host/spaces/index.vue"
+import HostSpaceListing from "./views/host/spaces/listing.vue"
+import HostShow from "./views/host/spaces/show.vue"
 import Error404 from "./views/pages/404.vue"
 
 const router = createRouter({
@@ -10,7 +12,9 @@ const router = createRouter({
   routes: [
     { path: '/', component: LandingPage, name: 'root_path' },
     { path: '/users/sign_up', component: Registration, name: 'registration_path' },
-    { path: '/spaces', component: HostIndex, name: 'spaces_path' },
+    { path: '/host/index', component: HostIndex, name: 'host_index_path' },
+    { path: '/host/listing', component: HostSpaceListing, name: 'host_listing_path' },
+    { path: '/host/show/:id', component: HostShow, name: 'host_show_path' },
     { path: '/404', component: Error404 },
     //{ path: '/pages', component: PageIndex, name: 'pages_path' },
     //{ path: '/musicians', component: MusicianIndex, name: 'musicians_path' },
