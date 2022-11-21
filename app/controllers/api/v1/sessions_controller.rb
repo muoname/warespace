95b107563b944c8eb7fcc8217ca7123419b1099d
regina-sv <rvibar@gbox.adnu.edu.ph>
2022-11-21 09:23:43 +0800

    users api json

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::SessionsController < ApplicationController
+
+
+    def index
+        render json: User.all.to_json(:only => [ :id, :first_name, :last_name, :address, :phone_number])
+    end
+
+
+end
