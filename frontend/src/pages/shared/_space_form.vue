b037a8e4cf6aa0ab4de489860d2fd34b76363179
unknown <easpe@gbox.adnu.edu.ph>
2022-12-02 17:01:04 +0800

    Paayos po Contoller

--- frontend/src/pages/shared/_space_form.vue
+++ frontend/src/pages/shared/_space_form.vue
@@ -58,13 +58,10 @@
           />
         </q-card-section>
         <q-card-actions align="right" class="text-primary">
-          <q-btn
-            color="primary"
-            type="submit"
-            label="Post Listing"
-            v-close-popup
-          />
+            <q-btn color="primary" type="submit" label="Post Listing" v-close-popup />
+
           <q-btn flat label="Cancel" v-close-popup />
+          
         </q-card-actions>
         <div class="q-pb-xs"></div>
       </q-form>
@@ -79,10 +76,17 @@ import "../../store";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
+  computed: {
+    ...mapGetters([
+      "getUserID",,
+    ]),
+  },
+
   data() {
     return {
       space: {},
       categories: {},
+      space_id: "",
       spacecategory_id: "",
       title: "",
       location: "",
@@ -97,20 +101,32 @@ export default {
       .get("http://localhost:3000/api/v1/spacecategories")
       .then((response) => {
         this.categories = response.data;
-        console.log(this.categories.data);
       });
+    axios.get("http://localhost:3000/api/v1/spaces/new").then((response) => {
+      console.log(response.data)
+    })
   },
 
   methods: {
     onSubmit(event) {
       event.preventDefault();
-      axios.post("http://localhost:3000/api/v1/spaces", {
-        spacecategory_id: this.spacecategory_id,
-        title: this.title,
-        location: this.location,
-        description: this.description,
-        space_size: this.space_size,
-        weekly_rate: this.weekly_rate,
+      new Promise((resolve, reject) => {
+        axios.post("http://localhost:3000/api/v1/spaces", {
+          user_id: this.getUserID,
+          spacecategory_id: this.spacecategory_id,
+          title: this.title,
+          location: this.location,
+          description: this.description,
+          space_size: this.space_size,
+          weekly_rate: this.weekly_rate,
+        })
+          .then((response) => {
+            commit("setUserInfo", response);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
       });
     },
   },
